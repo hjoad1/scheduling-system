9dec3d706a70626626580ff2c4cc2f7ef2708151
Huda Joad <hjoad@andrew.cmu.edu>
2025-01-22 02:04:58 +0300

    created views

--- scheduler/views.py
+++ scheduler/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Tutor, Student, Availability, Booking
+from .serializers import TutorSerializer, StudentSerializer, AvailabilitySerializer, BookingSerializer
 
 # Create your views here.
+
+class TutorViewSet(viewsets.ModelViewSet):
+    queryset = Tutor.objects.all()
+    serializer_class = TutorSerializer
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class AvailabilityViewSet(viewsets.ModelViewSet):
+    queryset = Availability.objects.all()
+    serializer_class = AvailabilitySerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    
