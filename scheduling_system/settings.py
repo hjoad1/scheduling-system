1b886b59745288add7853db06ecbe2fe42283d6e
Huda Joad <hjoad@andrew.cmu.edu>
2025-01-21 12:28:05 +0300

    set up postgres database

--- scheduling_system/settings.py
+++ scheduling_system/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'scheduling_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'scheduler_db',
+        'USER': 'scheduler_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
